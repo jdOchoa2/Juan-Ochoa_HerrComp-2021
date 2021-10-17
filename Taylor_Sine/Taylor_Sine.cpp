1926a9441989545cc1131284039af7c2c0fcecb9
Juan Ochoa <juochoaa@unal.edu.co>
2021-10-17 15:02:29 -0500

    porcentual error

--- Taylor_Sine/Taylor_Sine.cpp
+++ Taylor_Sine/Taylor_Sine.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cmath>
 
-double mysin(double x, int N, double []);                  //Returns the aproximation of sin(x) with N terms
+double mysin(double x, int N, double []);                      //Returns the aproximation of sin(x) with N terms
   
 int main()
 {
@@ -10,17 +10,17 @@ int main()
 
   const double x = M_PI/3;
   const double exact = std::sin(x);
-  const int NMAX = 10000;
+  const int NMAX = 1000;
 
   double terms[NMAX]={x};
-  double diff = std::fabs(mysin(x,1,terms)-exact)/exact;   //Return relative error
-  std::cout << "1" << "\t" << diff << "\n";                //Print the relative error with N=1
+  double diff = 100*std::fabs(mysin(x,1,terms)-exact)/exact;   //Return relative error
+  std::cout << "1" << "\t" << diff << "\n";                    //Print the relative error with N=1
   
   for(int N  = 2; N <= NMAX; N++)
     {
-      terms[N-1]=-terms[N-2]*std::pow(x,2)/(4*N*N-6*N+2);  //Returns the Nth term of the Taylor expansion using the previus one
-      diff = std::fabs(mysin(x,N,terms)-exact)/exact;      //Return relative error
-      std::cout << N << "\t" << diff << "\n";              //Print the relative error with N terms
+      terms[N-1]=-terms[N-2]*std::pow(x,2)/(4*N*N-6*N+2);     //Returns the Nth term of the Taylor expansion using the previus one
+      diff =100*std::fabs(mysin(x,N,terms)-exact)/exact;      //Return relative error
+      std::cout << N << "\t" << diff << "\n";                 //Print the relative error with N terms
     }
   return 0;
 }
@@ -28,7 +28,7 @@ int main()
 double mysin(double x, int N, double a[])
 {
   double suma = 0.0;
-  for(int n = N-1; n >= 0; n--)                            //Sums the terms backwrds
+  for(int n = N-1; n >= 0; n--)                               //Sums the terms backwrds
     {
       suma += a[n];
     }
