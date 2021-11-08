2e58660736a511ed691e2fa51c4f1887ae905d1e
Ochoa <juochoaa@unal.edu.co>
2021-11-07 22:32:04 -0500

    corrction complete

--- Debugger/debugging.cpp
+++ Debugger/debugging.cpp
@@ -9,37 +9,51 @@ void print_array(const double data[], const int & size);
 
 int main (int argc, char **argv)
 {
-  int ii, jj;
-  ii =  0;
-  jj = -1;
-  foo(ii, jj);
-  foo(jj, ii);
+  int a, b;
+  a =  0;
+  b = -1;
 
+  foo(a, b);
+  foo(b, a);
   baz(25.9);
 
-  const int NX = 2, NY = 3, NZ = 4;
-  double *x, y[NY], z[NZ];
-  x = new double [NX];
+  const int NX = 2, NY = 3, NZ = 5;
+  double *x, y[NY]={0}, z[NZ]={0};
+  x = new double [NX]();
+
   print_array(x, NX);
   print_array(y, NY);
   print_array(z, NZ);
   std::cout << std::endl;
 
-  for (ii = 0; ii < NX; ++ii) {
-    x[jj] = ii;
+  for (int ii = 0; ii < NX; ++ii) {
+    x[ii] = ii;
   }
-  print_array(x, NZ);
+
+  print_array(x, NX);
   print_array(y, NY);
-  print_array(z, NZ + NY);
+  print_array(z, NZ);
   std::cout << std::endl;
-  for (jj = 0; jj < NY; ++jj) {
-    x[jj] = ii;
+
+  for (int jj = 0; jj < NY; ++jj) {
     y[jj] = jj;
   }
-  print_array(x-NY, NY);
-  print_array(y-NZ, NZ);
-  print_array(z + NZ + NY, NX);
+
+  print_array(x, NX);
+  print_array(y, NY);
+  print_array(z, NZ);
   std::cout << std::endl;
+
+  for (int kk = 0; kk < NZ; ++kk) {
+    z[kk] = kk;
+  }
+
+  print_array(x, NX);
+  print_array(y, NY);
+  print_array(z, NZ);
+  std::cout << std::endl;
+
+  delete [] x;
 
    return EXIT_SUCCESS;
 }
@@ -48,7 +62,7 @@ int foo(int a, int b)
 {
   if (a*b*bar(a,b) == 0)
     {
-      std::cout << "División por 0"<<std::endl;
+      std::cout << "ATENCIÓN: División por 0"<<std::endl;
       return 0;
     }
   else return a/b + b/bar(a, b) + b/a;
@@ -56,14 +70,17 @@ int foo(int a, int b)
 
 int bar(int a, int b)
 {
-  unsigned int c = a;
+  int c = std::abs(a);
   return c + a - b;
 }
 
 double baz(double x)
 {
-  if (x == 0) return x;
-  double v = 1-(x+1);
+  if (std::abs(x) < 0.0000001) return 1-(x+1);
+  if (x<0)
+  {
+    std::cout<<"ATENCIÓN: Raíz imaginaria"<<std::endl;
+  }
   return std::sqrt(x);
 }
 
